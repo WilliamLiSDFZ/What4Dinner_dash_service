e6cfcb83561b4aee2a4fc729b8640fb2f9975ce1
WilliamLiSDFZ <liyuze2004@gmail.com>
2026-06-02 14:58:20 -0700

    init

---
+++ src/main/java/today/what4dinner/what4dinner_dash_service/What4DinnerDashServiceApplication.java
@@ -0,0 +1,13 @@
+package today.what4dinner.what4dinner_dash_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What4DinnerDashServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(What4DinnerDashServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/today/what4dinner/what4dinner_dash_service/What4DinnerDashServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package today.what4dinner.what4dinner_dash_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What4DinnerDashServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
